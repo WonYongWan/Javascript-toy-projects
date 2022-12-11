541de5f8fbe8be64589c82fb6d03ed225db224fd
WonYongWan <wonxx777@gmail.com>
2022-12-11 20:07:40 +0900

    todolist 생성

--- Pagination/main.js
+++ Pagination/main.js
@@ -3,14 +3,18 @@ let contListWrap = document.querySelector('.cont');
 let pageListWrap = document.querySelector('.page .list');
 
 // 데이터의 총 개수
-let totalCount = data.length;
+let totalData = 60;
 // 페이지의 총 개수
-let totalPage = Math.ceil(totalCount / 8);
-
-// 한 화면에 보여질 페이지 개수
+let totalPage = Math.ceil(totalData / 8);
+// 한 화면에 보여질 페이지리스트 개수
 let currentPage = Math.ceil(totalPage / 5);
 
-
+for(let i = 0; i < totalPage; i++) {
+  if(pageListWrap.childNodes.length < 5) {
+    pageListWrap.innerHTML += `<li><a href="#none">${i + 1}</a></li>`;
+  }
+}
+console.log(pageListWrap.childNodes.length);
 
 // function pageIndex(length) {
 //   for(let i = 0; i < length; i++) {
---
+++ ToDoList/main.js
@@ -0,0 +1,39 @@
+// const toDoListWrap = document.getElementById('list');
+init()
+
+function init() {
+  document.getElementById('new_todo').addEventListener("keyup", inputValue);
+  document.getElementById('list').addEventListener("dblclick", editToDo);
+}
+
+function inputValue(e) {
+  if(e.keyCode === 13 && this.value !== "") {
+    addToDo(this.value);
+    this.value = "";
+  }
+}
+
+function addToDo(value) {
+  const ul = document.getElementById('list');
+  const li = document.createElement('li');
+  li.innerHTML = `<div class="view"><input type="text" value="${value}" disabled></div>`;
+  ul.appendChild(li);
+}
+
+function editToDo(e) {
+  let test = document.querySelectorAll('#list input');
+  for(let i of test) {
+    if(i.value !== e.target ) {
+      console.log('erew')
+    }
+  }
+  console.log(e.target)
+  // if(e.target.hasAttribute('disabled') !== true) {
+
+  // }
+  // console.log(e.target.hasAttribute('disabled'))
+  // // document.querySelectorAll('input').setAttribute('disabled')
+  e.target.removeAttribute('disabled')
+  // // document.querySelector('div.view > input').setAttribute('disabled')
+}
+
