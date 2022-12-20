4907d171aa97dd736f535bc33f6ce339cb12a1b0
WonYongWan <wonxx777@gmail.com>
2022-12-20 21:07:58 +0900

    Number Baseball 완성

--- NumberBaseball/main.js
+++ NumberBaseball/main.js
@@ -1,19 +1,66 @@
+let statPage = document.querySelector('.startPage');
 let resultList = document.querySelector('.resultList');
 let numList = document.querySelector('.numList');
 let submitBtn = document.querySelector('.submit');
 let billBoard = document.querySelector('#billBoard');
 let scoreBoard = document.querySelector('#scoreBoard');
+let scoreLi = document.querySelectorAll('.score > li');
 let baseball = document.querySelector('.baseball');
-let scoreText = document.querySelector('.scoreText')
+let scoreText = document.querySelector('.scoreText');
+let youWin = document.querySelector('.youWin');
+let gameOver = document.querySelector('.gameOver');
+let restart = document.querySelectorAll('.restart');
+let end = document.querySelectorAll('.end');
+
+// 시작 화면
+startPage()
+function startPage() {
+  let startPageH1 = document.querySelector('.startPage > h1');
+  startPageH1.innerHTML = '';
+  startPageH1.classList.remove('none');
+  let startBtn = document.querySelector('.startPage > .start');
+  let startTextNumber = 'NUMBER BASEBALL';
+  let i = 0;
+  
+  const textAni = setInterval(() => {
+    startPageH1.innerHTML += `<span>${startTextNumber[i++]}</span>`;
+    if(i === 6) {
+      startPageH1.innerHTML += '<br/>';
+    }
+    if(i >= startTextNumber.length) {
+      clearInterval(textAni);
+    }
+    if(i === 15) {
+      setTimeout(() => {
+        startPageH1.innerHTML = `NUMBER<br/>BASEBALL`;
+        startPageH1.style.animationName = 'startPageAni';
+        setTimeout(() => {
+          startPageH1.style.animationName = '';
+        }, 1000)
+      }, 200)
+    }
+  }, 120);
+  setTimeout(() => {
+    startBtn.classList.remove('none');
+  }, 3000);
+  startBtn.addEventListener('click', () => {
+    statPage.classList.add('none');
+    startPageH1.classList.add('none');
+    startBtn.classList.add('none');
+  });
+}
 
 // 정답
 let answer = [];
-for(let i = 0; i < 4; i++) {
-  let random = Math.floor(Math.random() * 9 + 1);
-  if(answer.indexOf(random) === -1) {
-    answer.push(random);
-  } else {
-    i--;
+init();
+function init() {
+  for(let i = 0; i < 4; i++) {
+    let random = Math.floor(Math.random() * 9 + 1);
+    if(answer.indexOf(random) === -1) {
+      answer.push(random);
+    } else {
+      i--;
+    }
   }
 }
 
@@ -84,19 +131,16 @@ function numCompare() {
   window.removeEventListener('mouseover', submitAni);
   billBoard.style.transform = 'translateY(-200px)';
   setTimeout(() => {
-    window.addEventListener('mouseover', submitAni);
     resultList.innerHTML = '';
+    setTimeout(() => {
+      window.addEventListener('mouseover', submitAni);
+    }, 2000)
   }, 100)
   for(let numItem of num) {
     numItem.classList.remove('choice');
     numItem.classList.add('normal');
   }
   turn += 1;
-  if(turn > 9) {
-    setTimeout(() => {
-      alert('게임종료')
-    }, 3000);
-  }
   let strike = 0;
   let ball = 0;
   submitBtn.classList.add('none');
@@ -106,18 +150,51 @@ function numCompare() {
       if(index === i) {
         strike += 1;
         if(strike === 4) {
-          alert('게임 승리');
+          setTimeout(() => {
+            youWin.classList.remove('none');
+            for(let i of restart) {
+              i.addEventListener('click', restartEvent);
+            }
+            for(let i of end) {
+              i.addEventListener('click', endEvent);
+            }
+          }, 3000);
         }
       } else {
         ball += 1;
       }
     }
   }
+  if(turn > 9 && strike !== 4) {
+    setTimeout(() => {
+      gameOver.classList.remove('none');
+      for(let i of restart) {
+        i.addEventListener('click', restartEvent);
+      }
+      for(let i of end) {
+        i.addEventListener('click', endEvent);
+      }
+    }, 3000);
+  } 
+  if((turn > 9 && strike === 4)) {
+    setTimeout(() => {
+      youWin.classList.remove('none');
+      for(let i of restart) {
+        i.addEventListener('click', restartEvent);
+      }
+      for(let i of end) {
+        i.addEventListener('click', endEvent);
+      }
+    }, 3000);
+  }
+  // answer test
+  console.log(answer)
   gameScore(turn, strike, ball);
   gameRecord(result);
   baseballAni(strike, ball);
   result = [];
 }
+
 // 제출 후 야구공 애니메이션
 function baseballAni(strike, ball) {
   if(strike === 0 && ball === 0) {
@@ -130,6 +207,17 @@ function baseballAni(strike, ball) {
       setTimeout(() => {
         scoreText.innerHTML = '';
       }, 1000)
+    }, 1200);
+  } else if(strike === 4) {
+    baseball.classList.add('ani1');
+    setTimeout(() => {
+      scoreText.innerHTML = `<span>OUT!!</span>`;
+    }, 800);
+    setTimeout(() => {
+      baseball.classList.remove('ani1');
+      setTimeout(() => {
+        scoreText.innerHTML = '';
+      }, 1000);
     }, 1200);
   } else {
     baseball.classList.add('ani1');
@@ -148,6 +236,50 @@ function baseballAni(strike, ball) {
     billBoard.classList.remove('submit');
     billBoard.style.transform = 'translateY(0)';
   }, 2000)
+}
+
+// 재시작
+function restartEvent() {
+  answer = [];
+  init();
+  result = [];
+  turn = 0;
+  strike = 0;
+  ball = 0;
+  record.innerHTML = '';
+  resultList.innerHTML = '';
+  for(let i of scoreLi) {
+    i.innerHTML = '';
+  }
+  for(let i of num) {
+    i.classList.remove('choice');
+    i.classList.add('normal');
+  }
+  youWin.classList.add('none');
+  gameOver.classList.add('none');
+}
+
+// 게임 종료 및 첫 화면
+function endEvent() {
+  answer = [];
+  init();
+  result = [];
+  turn = 0;
+  strike = 0;
+  ball = 0;
+  record.innerHTML = '';
+  resultList.innerHTML = '';
+  for(let i of scoreLi) {
+    i.innerHTML = '';
+  }
+  for(let i of num) {
+    i.classList.remove('choice');
+    i.classList.add('normal');
+  }
+  youWin.classList.add('none');
+  gameOver.classList.add('none');
+  statPage.classList.remove('none')
+  startPage();
 }
 
 // 전광판 스코어 정보
