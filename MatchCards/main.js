08d73e65f4c03fa72ceb5b12e7ce7532c9858574
WonYongWan <wonxx777@gmail.com>
2023-01-04 05:51:30 +0900

    reset화면 추가

--- MatchCards/main.js
+++ MatchCards/main.js
@@ -1,8 +1,16 @@
 const $wrap = document.querySelector('#wrap');
+const $success = document.querySelector('#success');
 
-createCards();
+let targetElms = [];
+let flag = 0;
+let score = 0;
+let startTime;
+let endTime;
+let totalTime;
 
+// 카드 생성
 function createCards() {
+  $wrap.removeEventListener('click', cardClickEvent);
   let numOfCards = 0;
   let backgroundColor = ['yellow', 'red', 'blue', 'white', 'gray', 'green'];
   let backgroundColorClone = backgroundColor.concat(backgroundColor).sort();
@@ -31,22 +39,22 @@ function createCards() {
           cards.forEach(elm => {
             elm.style.transform = '';
           });
-          let flag = 0;
-          let score = 0;
-          let targetElms = [];
-          $wrap.addEventListener('click', cardClickEvent(flag, targetElms, score));
+          $wrap.addEventListener('click', cardClickEvent);
         }, 1000);
       }
     }, 100); 
   }, 500);
 }
+createCards();
 
-const cardClickEvent = (flag, targetElms, score) => (e) => {
+// 클릭 이벤트
+function cardClickEvent(e) {
   let targetElm;
   if(e.target.parentNode !== document.body
     && e.target.parentNode !== $wrap
     && e.target.parentNode.style.transform !== 'rotateY(180deg)') {
 
+    if(!startTime) startTime = new Date();
     targetElm = e.target.parentNode;
     targetElms.push(targetElm);
     targetElm.style.transform = 'rotateY(180deg)';
@@ -68,4 +76,27 @@ const cardClickEvent = (flag, targetElms, score) => (e) => {
       }, 500);
     }
   }
+  if(score === 6) {
+    if(!endTime) endTime = new Date();
+    totalTime = ((endTime - startTime) / 1000).toFixed(1);
+    setTimeout(() => {
+      $success.classList.add('on');
+      $success.innerHTML += `<p>${totalTime}초 걸렸습니다!</p>`;
+      $success.innerHTML += '<button id="reset">RESET</button>';
+      const $reset = document.querySelector('#reset');
+      $reset.addEventListener('click', gameReset);
+    }, 1000);
+  }
+}
+
+// 게임 초기화
+function gameReset() {
+  startTime = null;
+  endTime = null;
+  totalTime = null;
+  $success.classList.remove('on');
+  $success.innerHTML = '';
+  $wrap.innerHTML = '';
+  score = 0;
+  createCards();
 }
