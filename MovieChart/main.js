28b50baae7277ad5c8b2f3e84f0acff18b5fa0bc
WonYongWan <wonxx777@gmail.com>
2023-01-03 19:35:50 +0900

    폴더 정리

--- MovieChart/main.js
+++
@@ -1,6 +0,0 @@
-async function getDate() {
-  const rawResponse = await fetch("https://openapi.naver.com/v1/search/movie.json")
-  const jsonResponse = await rawResponse.json();
-  console.log(jsonResponse)
-}
-getDate()
