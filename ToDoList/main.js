aeb4d3b234ab3bf8a19a2eb319fde073ddb8a909
WonYongWan <wonxx777@gmail.com>
2023-01-30 22:07:28 +0900

    ToDoList 기능 추가

--- ToDoList/main.js
+++ ToDoList/main.js
@@ -1,39 +1,60 @@
-// // const toDoListWrap = document.getElementById('list');
-// init()
+const $newTodo = document.querySelector('#new_todo');
+const $itemList = document.querySelector('.item_list');
 
-// function init() {
-//   document.getElementById('new_todo').addEventListener("keyup", inputValue);
-//   document.getElementById('list').addEventListener("dblclick", editToDo);
-// }
+$newTodo.addEventListener('keyup', inputValueProcess);
 
-// function inputValue(e) {
-//   if(e.keyCode === 13 && this.value !== "") {
-//     addToDo(this.value);
-//     this.value = "";
-//   }
-// }
+// 내용 입력시 Enter Key만 허용, 공백문자 차단, 텍스트 사이 공백 조정, 텍스트 앞 뒤 공백 제거
+function inputValueProcess(e) {
+  if(e.keyCode !== 13) return;
+  if(this.value === '') return;
+  let valueTexts = this.value;
+  let blankCount = 0;
+  for(let i of valueTexts) {if(i.charCodeAt() === 32) blankCount++;}
+  if(blankCount === valueTexts.length) return;
+  valueTexts = valueTexts.replace(/ +/g, " ");
 
-// function addToDo(value) {
-//   const ul = document.getElementById('list');
-//   const li = document.createElement('li');
-//   li.innerHTML = `<div class="view"><input type="text" value="${value}" disabled></div>`;
-//   ul.appendChild(li);
-// }
+  let inputValue = valueTexts.trim();
+  CreateItem(inputValue);
+  $newTodo.value = '';
+}
 
-// function editToDo(e) {
-//   let test = document.querySelectorAll('#list input');
-//   for(let i of test) {
-//     if(i.value !== e.target ) {
-//       console.log('erew')
-//     }
-//   }
-//   console.log(e.target)
-//   // if(e.target.hasAttribute('disabled') !== true) {
+// $itemList에 item 제작 및 삽입
+function CreateItem(inputValue) {
+  const $item = document.createElement('div');
+  $item.setAttribute('class', 'item');
 
-//   // }
-//   // console.log(e.target.hasAttribute('disabled'))
-//   // // document.querySelectorAll('input').setAttribute('disabled')
-//   e.target.removeAttribute('disabled')
-//   // // document.querySelector('div.view > input').setAttribute('disabled')
-// }
+  const $p = document.createElement('p');
+  $p.setAttribute('class', 'on');
+  $p.innerText = inputValue;
+
+  const $input = document.createElement('input');
+  $input.setAttribute('type', 'text');
+
+  const $button = document.createElement('button');
+  $button.innerText = 'X';
+
+  $item.append($p, $input, $button);
+  $itemList.append($item);
+
+  // P태그 더블클릭 이벤트!
+  const $itemsPTag = document.querySelectorAll('.item p');
+  $itemsPTag.forEach(elm => elm.addEventListener('click', itemTextModifyStart));
+}
+
+// P태그 더블클릭 이벤트!
+function itemTextModifyStart(e) {
+  const $itemInputTag = e.target.nextSibling;
+
+  e.target.classList.remove('on');
+  $itemInputTag.value = e.target.innerText;
+  $itemInputTag.classList.add('on');
+}
+
+function itemCompleted() {
+
+}
+
+function stateOfItemLeft() {
+
+}
 
